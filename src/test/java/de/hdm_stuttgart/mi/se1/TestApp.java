91e0c52e4bb1ba74937e9fa4d14c054e6183c3db
Zobel Linda Maria Christina <lz020@hdm-stuttgart.de>
2018-01-04 16:54:12 +0100

    testing basic structure set up

---
+++ src/test/java/de/hdm_stuttgart/mi/se1/TestApp.java
@@ -0,0 +1,26 @@
+package de.hdm_stuttgart.mi.se1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import de.hdm_stuttgart.mi.se1.*;
+
+/**
+ * Unit tests
+ */
+public class TestApp {
+
+    /**
+     * Testing simple addition.
+     */
+    @Test
+    public void testSimpleAddition(){
+        //assertEquals(5, SetupBot.stringFilterOperatorsIntoArrayAndEverythingElseIntoStack("2 3 +"));
+    }
+    /**
+     * Testing simple subtraction.
+     */
+    @Test
+    public void testSimpleSubtraction(){
+        //assertArrayEquals(5, MathOperation.add("2 3 -"));
+    }
+}
